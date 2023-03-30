12a13c481afb7567d8a67e80fffeb21f78a000ec
Joanna0923 <markowsk.asia@gmail.com>
2023-03-30 20:07:22 +0200

    komentarze rozuchowe

--- portfolio/portfolio/main.py
+++ portfolio/portfolio/main.py
@@ -2,3 +2,15 @@ import selenium
 
 print("hello world")
 
+# zrobić resztę - napisać kod selenium który wejdzie na tą stronkę wystawioną i wpisze coś w formularzu
+# dodawanie zależności na video się klikało tutaj się robi poetry add <nazwa paczki>
+# np `pip install Faker` zamianiamy na `poetry add Faker`
+# jak uruchomisz od nowa vscode to żeby wejść do środowiska wirtualnego trzeba wspiać `poetry shell`` (znajdując się w katalogu głównym projektu)
+# aby przejść do katalogu głównego projektu wpisz `cd /home/joanna/Portfolio/portfolio`
+# (portfolio-py3.10) przed zielonym oznacza że jestś w środowisku wirtualnym
+# wykonanie kodu: python portfolio/main.py (poza środowiskiem wirtualnym może nie działać)
+
+#kolejność
+#1. `cd /home/joanna/Portfolio/portfolio`
+#2. `poetry shell
+#3. python portfolio/main.py
